767c2c56cf607d66f476e49c1039451f9773d978
drinkcoffee <dcoffie92@gmail.com>
2020-01-18 00:04:18 -0800

    connected front-end to back-end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Movies } from './components/Movies';
+import { MovieForm } from './components/MovieForm';
+import { Container } from 'semantic-ui-react';
 
 function App() {
+
+  const [movies, setMovies] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:5000/movies', {
@@ -13,14 +18,19 @@ function App() {
     })
       .then(response => response.json()
         .then(data => {
-          console.log(data)
+          setMovies(data.movies)
         })
       )
   }, [])
 
-  return <div className="App" />
-
-
+  return (
+    <div className="App">
+      <Container style={{ marginTop: 40 }}>
+        <MovieForm onNewMovie={movie => setMovies(currentMovies => [movie, ...currentMovies])} />
+        <Movies movies={movies} />
+      </Container>
+    </div>
+  )
 }
 
 export default App;
